3d5f6bd5f01e36dc7fcf6223d97cce260a309384
Andreas Dolp <andi1997@t-online.de>
2016-12-11 02:19:57 +0100

    finished the state manager

--- DialogSystem/src/de/roboy/dialog/personality/GuessingGamePersonality.java
+++ DialogSystem/src/de/roboy/dialog/personality/GuessingGamePersonality.java
@@ -1,6 +1,7 @@
 package de.roboy.dialog.personality;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import de.roboy.dialog.action.Action;
 import de.roboy.dialog.action.EmotionAction;
 import de.roboy.dialog.action.ShutDownAction;
 import de.roboy.dialog.action.SpeechAction;
+import de.roboy.guessinggame.Association;
 import de.roboy.io.ClassificationInput;
 import de.roboy.io.Input;
 import de.roboy.io.SentenceInput;
@@ -23,21 +25,30 @@ public class GuessingGamePersonality implements Personality {
 		WELCOME, REGISTER_GROUP_A, REGISTER_GROUP_B, REGISTER_TERM, REGISTER_STOP_WORDS, FINALIZE_SETUP, GROUP_A_SENTENCE_INPUT, GROUP_B_SENTENCE_INPUT, TELL_RESULTS
 	}
 
+	private final static int GROUP_A = 0;
+	private final static int GROUP_B = 1;
+	private final static double POINT_BORDER = 1; // TODO set border
 	private GuessingGameState state = GuessingGameState.WELCOME;
 	private String groupA = "";
 	private String groupB = "";
+	private boolean groupAsTurnFirst = true;
+	private boolean firstRound = true;
 	private String term = "";
 	private HashSet<String> stopWords = new HashSet<String>();
 	private ClassificationInput classification;
 	private SentenceInput sentence;
+	private Association association; // at Index 0: Nouns; Index 1: Verbs; Index
+										// 2: Adjectives
+	private int pointsA = 0;
+	private int pointsB = 0;
 
 	@Override
 	public List<Action> answer(Interpretation input) {
 		List<Action> result = new ArrayList<Action>();
-		
 		/*
-		 * At the beginning of a state the input for the previous state gets
-		 * processed and if the input is not valid, it repeats this state
+		 * TODO Emotion cycle At the beginning of a state the input for the
+		 * previous state gets processed and if the input is not valid, it
+		 * repeats this state
 		 */
 		switch (state) {
 		case WELCOME:
@@ -93,12 +104,42 @@ public class GuessingGamePersonality implements Personality {
 				return result;
 			}
 
+			// TODO ask if not first round if they want to continue playing,
+			// tell current points
+			if (!firstRound) {
+				groupAsTurnFirst = !groupAsTurnFirst;
+				result.add(new SpeechAction("Currently " + groupA + " has " + pointsA + " points. " + groupB + " has "
+						+ pointsB + " points."));
+				if (sentence != null) {
+					if (sentence.getInput().toLowerCase().contains("yes")) {
+						// play another round
+						result.add(new SpeechAction(
+								"Let's play another round. Please tell me the object yout want me to guess."));
+						term = "";
+						state = GuessingGameState.REGISTER_STOP_WORDS;
+						return result;
+					} else if (sentence.getInput().toLowerCase().contains("no")) {
+						// go to TELL_RESULTS
+						result.add(new SpeechAction("Thank you for playing with me. Did you enjoy it?"));
+						state = GuessingGameState.TELL_RESULTS;
+						return result;
+					} else {
+						// ask again
+						result.add(new SpeechAction("I did not understand you. Do you want to play another round?"));
+						state = GuessingGameState.REGISTER_TERM;
+						return result;
+					}
+				}
+			}
+
 			// Ask for Term
 			result.add(new SpeechAction("Please tell me the object yout want me to guess."));
 			state = GuessingGameState.REGISTER_STOP_WORDS;
 			return result;
 
 		case REGISTER_STOP_WORDS:
+			// once they got here, they won't get back to REGISTER_TERM
+			firstRound = false;
 			// check input for term
 			if (sentence == null) {
 				result.add(new SpeechAction("You did not say an object. You don't want to play with me."));
@@ -112,6 +153,9 @@ public class GuessingGamePersonality implements Personality {
 					int iSeeIt;
 					if (classification.getProbabilities().containsKey(sentence.getInput())) {
 						term = sentence.getInput();
+						// call python script to get associations and process
+						// them
+						association = getAssociations(term);
 						result.add(
 								new SpeechAction("I see " + term + ". But I will forget that you told me what it is."));
 						result.add(new EmotionAction("happy"));// TODO make him
@@ -143,38 +187,202 @@ public class GuessingGamePersonality implements Personality {
 		case FINALIZE_SETUP:
 			// save the stopwords
 			if (sentence != null) {
-				String[] arr = sentence.getInput().split("\\s+|,"); // TODO
+				String[] arr = sentence.getInput().split("\\s+"); // TODO
 																	// regex
 				// the term is also not allowed to say
 				stopWords.add(term);
 				String allStopWords = term;
 				for (int i = 0; i < arr.length; ++i) {
-					arr[i].replaceAll("\\s+|,", "");
+					arr[i] = arr[i].replaceAll("\\s+|,", "");
 					stopWords.add(arr[i]);
 					allStopWords = allStopWords + ", " + arr[i];
 				}
 				result.add(new SpeechAction("The words you are not allowed to use are: " + allStopWords + "."));
 			}
-			result.add(new SpeechAction(groupA + ", please describe the object."));
-			state = GuessingGameState.GROUP_A_SENTENCE_INPUT;
-			return result;
+			if (groupAsTurnFirst) {
+				result.add(new SpeechAction(groupA + ", please describe the object."));
+				state = GuessingGameState.GROUP_A_SENTENCE_INPUT;
+				return result;
+			} else {
+				result.add(new SpeechAction(groupB + ", please describe the object."));
+				state = GuessingGameState.GROUP_B_SENTENCE_INPUT;
+				return result;
+			}
 		case GROUP_A_SENTENCE_INPUT:
+			if (sentence == null) {
+				result.add(new SpeechAction(groupA + " didn't even try."));
+				result.add(new EmotionAction("sad"));
+				if (!groupAsTurnFirst) {
+					// groupB started this round -> nobody could answer right
+					return nobodyGotIt(result);
+				} else {
+					// groupA started this round -> groupBs turn now
+					result.add(new SpeechAction(groupB + ", please describe the object."));
+					state = GuessingGameState.GROUP_B_SENTENCE_INPUT;
+					return result;
+				}
+			} else {
+				List<String> hintsList = Arrays.asList(sentence.getInput().split("\\s+"));
+				for (int i = 0; i < hintsList.size(); ++i) {
+					hintsList.set(i, hintsList.get(i).replaceAll("\\s+|,", ""));
+					if (stopWords.contains(hintsList.get(i))) {
+						// They used a stopWord
+						result.add(new SpeechAction(
+								groupA + " used a word that was not allowed. The other team will get a point."));
+						result.add(new EmotionAction("sad"));
+						addPoint(GROUP_B);
+						result.add(new SpeechAction("Do you want to play antoher round?"));
+						state = GuessingGameState.REGISTER_TERM;
+						return result;
+					}
+				}
+				double prob = association.getProbability(hintsList);
+				if (prob < POINT_BORDER) {
+					// score too low
+					if (!groupAsTurnFirst) {
+						// groupB started this round -> nobody answered right
+						return nobodyGotIt(result);
+					} else {
+						// groupA started this round -> groupBs turn now
+						result.add(new SpeechAction(
+								groupA + ", your hints didn't really make clear to me what you were talking about."));
+						result.add(new SpeechAction(groupB + ", please describe the object."));
+						state = GuessingGameState.GROUP_B_SENTENCE_INPUT;
+						return result;
+					}
+				} else {
+					// score high enough -> point for groupA
+					result.add(
+							new SpeechAction(groupA + ", you clearly described " + term + ". You will get one point."));
+					result.add(new EmotionAction("happy"));
+					addPoint(GROUP_A);
+					result.add(new SpeechAction("Do you want to play antoher round?"));
+					state = GuessingGameState.REGISTER_TERM;
+					return result;
+				}
+			}
 		case GROUP_B_SENTENCE_INPUT:
+			if (sentence == null) {
+				result.add(new SpeechAction(groupB + " didn't even try."));
+				result.add(new EmotionAction("sad"));
+				if (!groupAsTurnFirst) {
+					// groupB started this round -> groupAs turn now
+					result.add(new SpeechAction(groupA + ", please describe the object."));
+					state = GuessingGameState.GROUP_A_SENTENCE_INPUT;
+					return result;
+				} else {
+					// groupA started this round -> nobody got it
+					return nobodyGotIt(result);
+				}
+			} else {
+				List<String> hintsList = Arrays.asList(sentence.getInput().split("\\s+"));
+				for (int i = 0; i < hintsList.size(); ++i) {
+					hintsList.set(i, hintsList.get(i).replaceAll("\\s+|,", ""));
+					if (stopWords.contains(hintsList.get(i))) {
+						// They used a stopWord
+						result.add(new SpeechAction(
+								groupB + " used a word that was not allowed. The other team will get a point."));
+						result.add(new EmotionAction("sad"));
+						addPoint(GROUP_A);
+						result.add(new SpeechAction("Do you want to play antoher round?"));
+						state = GuessingGameState.REGISTER_TERM;
+						return result;
+					}
+				}
+				double prob = association.getProbability(hintsList);
+				if (prob < POINT_BORDER) {
+					// score too low
+					if (!groupAsTurnFirst) {
+						// groupB started this round -> groupAs turn now
+						result.add(new SpeechAction(
+								groupB + ", your hints didn't really make clear to me what you were talking about."));
+						result.add(new SpeechAction(groupA + ", please describe the object."));
+						state = GuessingGameState.GROUP_A_SENTENCE_INPUT;
+						return result;
+					} else {
+						// groupA started this round -> nobody answered right
+						return nobodyGotIt(result);
+					}
+				} else {
+					// score high enough -> point for groupB
+					result.add(
+							new SpeechAction(groupB + ", you clearly described " + term + ". You will get one point."));
+					result.add(new EmotionAction("happy"));
+					addPoint(GROUP_B);
+					result.add(new SpeechAction("Do you want to play antoher round?"));
+					state = GuessingGameState.REGISTER_TERM;
+					return result;
+				}
+			}
 		case TELL_RESULTS:
-		default:
 			List<Action> lastwords = new ArrayList<Action>();
-			lastwords.add(new SpeechAction("I am lost, bye."));
+			if (pointsA > pointsB) {
+				// group A won
+				lastwords.add(new SpeechAction("The winner is " + groupA + " with " + pointsA + " points. " + groupB
+						+ " got " + pointsB + " points."));
+			} else if (pointsA < pointsB) {
+				// group B won
+				lastwords.add(new SpeechAction("The winner is " + groupB + " with " + pointsB + " points. " + groupA
+						+ " got " + pointsA + " points."));
+			} else {
+				// tied
+				lastwords.add(new SpeechAction(groupA + " and " + groupB + " both got " + pointsA + " points."));
+			}
 			result.add(new ShutDownAction(lastwords));
+			return result;
+
+		default:
+			List<Action> mylastwords = new ArrayList<Action>();
+			mylastwords.add(new SpeechAction("I am lost, bye."));
+			result.add(new ShutDownAction(mylastwords));
 			return result;
 		}
 	}
 
-	public void setSentence(SentenceInput sentence){
+	public void setSentence(SentenceInput sentence) {
 		this.sentence = sentence;
 	}
-	public void setClassification(ClassificationInput classification){
+
+	public void setClassification(ClassificationInput classification) {
 		this.classification = classification;
 	}
+
+	private Association getAssociations(String object) {
+		String[] arr = new String[3];
+		String[] arrNouns, arrVerbs, arrAdj;
+		String strPython;
+		// call python script; python script returns String with 3 Lines ->
+		// separate by \\n?
+		strPython = "Macintosh Cider Pear Plum Orchard Peach Pie Atari App Os Raspberry Pineapple Raisin Blossom Samsung Strawberry Juice Fiona Cherry Amiga Cultivar Cinnamon Mango Melon Grape Banana Mac Pudding Discord Fruit Beatles Turnip Chestnut Sauce Lemon Cucumber Crab Safari Cabbage Almond Ibm Carrot Adam Dessert Commodore Laptop Slice Beet Intel Bough Potato Jelly Pumpkin Adobe Keynote Syrup Vinegar Android Flavour Butter Onion Nokia Mulberry Heracles Bake Lime Nut Gui Orange Aphrodite Pc Hera Peel Grower Seedling Flavor Pea Tomato Candy Processor Dos\n"
+				+ "Cherry Android Ripe Desktop Sour Roast Citrus Plum Stereo Roasted Rotten Peeled\n"
+				+ "Bake Bob Pare Slice Port Chop Infringe";
+
+		arr = strPython.split("\\n");
+		arrNouns = arr[0].split("\\s+");
+		arrVerbs = arr[1].split("\\s+");
+		arrAdj = arr[2].split("\\s+");
+		Association objAssociations = new Association(Arrays.asList(arrNouns), Arrays.asList(arrVerbs),
+				Arrays.asList(arrAdj));
+		return objAssociations;
+	}
+
+	private void addPoint(int group) {
+		if (group == GROUP_A) {
+			++pointsA;
+		} else {
+			++pointsB;
+		}
+	}
+
+	private List<Action> nobodyGotIt(List<Action> result) {
+		result.add(new SpeechAction("Nobody described the Object good enough for me to recognize it. "
+				+ "Maybe I am just not smart enough"));
+		result.add(new EmotionAction("sad"));
+		result.add(new SpeechAction("Do you want to play antoher round?"));
+		state = GuessingGameState.REGISTER_TERM;
+		return result;
+	}
+
 	private void filterSpamWords() {
 		// TODO take out words like 'and', 'or', .. of the sentence and return
 		// the filtered sentence
---
+++ DialogSystem/src/de/roboy/guessinggame/Association.java
@@ -0,0 +1,37 @@
+package de.roboy.guessinggame;
+
+import java.util.List;
+
+public class Association {
+	private List<String> nouns;
+	private List<String> verbs;
+	private List<String> adjectives;
+
+	public Association(List<String> nouns, List<String> verbs, List<String> adjectives) {
+		this.nouns = nouns;
+		this.verbs = verbs;
+		this.adjectives = adjectives;
+	}
+
+	public double getProbability(List<String> hints) { // TODO which parameters?
+		double result, position, length;
+		result = 0;
+		for (int i = 0; i < hints.size(); ++i) {
+			if (nouns.contains((hints.get(i)))) {
+				position = nouns.indexOf(hints.get(i));
+				length = nouns.size();
+			} else if (verbs.contains((hints.get(i)))) {
+				position = verbs.indexOf(hints.get(i));
+				length = verbs.size();
+			} else if (adjectives.contains((hints.get(i)))) {
+				position = adjectives.indexOf(hints.get(i));
+				length = adjectives.size();
+			} else {
+				continue;
+			}
+			result += 1 - (position / length);
+		}
+		//result = result / hints.size();
+		return result;
+	}
+}
